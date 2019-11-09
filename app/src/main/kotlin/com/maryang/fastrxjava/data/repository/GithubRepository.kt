161e0295d1dddca5f119e0c1ea0434bc2672924e
oh3vci <oh3vci@gmail.com>
2019-11-09 16:43:40 +0900

    Use Single class of Observable instead of Call of retrofit2

--- app/src/main/kotlin/com/maryang/fastrxjava/data/repository/GithubRepository.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/data/repository/GithubRepository.kt
@@ -2,12 +2,12 @@ package com.maryang.fastrxjava.data.repository
 
 import com.maryang.fastrxjava.data.source.ApiManager
 import com.maryang.fastrxjava.entity.GithubRepo
-import retrofit2.Call
+import io.reactivex.Single
 
 class GithubRepository {
 
     private val api = ApiManager.githubApi
 
-    fun getGithubRepos(): Call<List<GithubRepo>> =
+    fun getGithubRepos(): Single<List<GithubRepo>> =
         api.getRepos()
 }
